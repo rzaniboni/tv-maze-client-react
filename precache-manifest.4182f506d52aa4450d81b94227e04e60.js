0170487b75f81ce0a4b5b1f422556d4c4fb0b80b
Roberto Zaniboni <zaniboni@gmail.com>
2020-06-20 11:32:57 +0200

    Updates

---
+++ precache-manifest.4182f506d52aa4450d81b94227e04e60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a8c76130d93a43c03e1dc2e23f48519",
+    "url": "/tv-maze-client-react/index.html"
+  },
+  {
+    "revision": "9379968455e79fb879bc",
+    "url": "/tv-maze-client-react/static/css/main.cce7cd49.chunk.css"
+  },
+  {
+    "revision": "305d97ae15c595fcbed4",
+    "url": "/tv-maze-client-react/static/js/2.3cceb7bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tv-maze-client-react/static/js/2.3cceb7bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9379968455e79fb879bc",
+    "url": "/tv-maze-client-react/static/js/main.0ccc59a8.chunk.js"
+  },
+  {
+    "revision": "02df793358061f7ea378",
+    "url": "/tv-maze-client-react/static/js/runtime-main.fadd44dd.js"
+  }
+]);
